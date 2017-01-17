185f82450f4036ebf43b38d9b0bd706bbe9ae277
c3bola <dev@macUan.home>
2017-01-17 09:32:34 -0200

    Change config.php
    Change database anda key

--- function/config.php
+++ function/config.php
@@ -1,13 +1,8 @@
 <?php
-/*DEFINE ('TIPOBANCO', 'mysql');
+DEFINE ('TIPOBANCO', 'mysql');
 DEFINE ('SERVIDOR', 'localhost');
 DEFINE ('USUARIO','root');
-DEFINE ('SENHA','123');
-DEFINE ('BANCO','simgetec');
-/*/
-DEFINE ('TIPOBANCO', 'mysql');
-DEFINE ('SERVIDOR', 'simgetec.mysql.dbaas.com.br');
-DEFINE ('USUARIO','simgetec');
-DEFINE ('SENHA','i3M{ds73f@kdn');
-DEFINE ('BANCO','simgetec');
+DEFINE ('SENHA','');
+DEFINE ('BANCO','PHPConecta');
+
 ?>
