049464194a4943e2c4a79e65a1ecd066c0a8ad1a
c3bola <dev@macUan.home>
2017-01-17 09:26:26 -0200

    First deploy
    First deploy the application

---
+++ function/config.php
@@ -0,0 +1,13 @@
+<?php
+/*DEFINE ('TIPOBANCO', 'mysql');
+DEFINE ('SERVIDOR', 'localhost');
+DEFINE ('USUARIO','root');
+DEFINE ('SENHA','123');
+DEFINE ('BANCO','simgetec');
+/*/
+DEFINE ('TIPOBANCO', 'mysql');
+DEFINE ('SERVIDOR', 'simgetec.mysql.dbaas.com.br');
+DEFINE ('USUARIO','simgetec');
+DEFINE ('SENHA','i3M{ds73f@kdn');
+DEFINE ('BANCO','simgetec');
+?>
